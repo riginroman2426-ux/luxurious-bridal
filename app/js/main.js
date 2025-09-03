b182035f3bfbe7cba8f7facdd92c04b5dc5a7786
riginroman2426-ux <riginroman2426@gmail.com>
2025-09-03 23:51:49 +0300

    added reviews page

--- app/js/main.js
+++ app/js/main.js
@@ -7,3 +7,17 @@ const swiper = new Swiper(".accessories__slider", {
     prevEl: ".arrow-prev",
   },
 });
+
+const swiperReviews = new Swiper(".reviews-slider", {
+  slidesPerView: 12,
+  spaceBetween: 16,
+  loop: true,
+  navigation: {
+    nextEl: ".reviews-slider-next",
+    prevEl: ".reviews-slider-prev",
+  },
+  pagination: {
+    el: ".reviews-slider-pagination",
+    type: "fraction",
+  },
+});
