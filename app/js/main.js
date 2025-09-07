b481aecb19cd8c1f6890d13b5f3a121f66dd1a04
riginroman2426-ux <riginroman2426@gmail.com>
2025-09-07 22:26:42 +0300

    Оновлення: нові сторінки та картинки, зміни у стилях і скриптах

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
+const viewLine = document.querySelector(".view-mod__btn--line");
+const viewGrid = document.querySelector(".view-mod__btn--grid");
+const viewContainer = document.querySelector(".view-mod__container");
+
+viewGrid.addEventListener("click", () => {
+  viewContainer.classList.add("view-mod__container--grid");
+  viewContainer.classList.remove("view-mod__container--line");
+});
+
+viewLine.addEventListener("click", () => {
+  viewContainer.classList.add("view-mod__container--line");
+  viewContainer.classList.remove("view-mod__container--grid");
+});
+
 const swiper = new Swiper(".accessories__slider", {
   slidesPerView: 3,
   spaceBetween: 40,
@@ -9,7 +23,7 @@ const swiper = new Swiper(".accessories__slider", {
 });
 
 const swiperReviews = new Swiper(".reviews-slider", {
-  slidesPerView: 12,
+  slidesPerView: 8,
   spaceBetween: 16,
   loop: true,
   navigation: {
@@ -20,4 +34,35 @@ const swiperReviews = new Swiper(".reviews-slider", {
     el: ".reviews-slider-pagination",
     type: "fraction",
   },
+});
+
+const rangeSlider = document.querySelector(".range__slider");
+const inputMin = document.querySelector(".range__min");
+const inputMax = document.querySelector(".range__max");
+noUiSlider.create(rangeSlider, {
+  start: [300, 3000],
+  step: 100,
+  range: {
+    min: 300,
+    max: 3000,
+  },
+  format: {
+    to: (value) => Math.round(value),
+    from: (value) => Number(value),
+  },
+});
+
+rangeSlider.noUiSlider.on("update", (values, handle) => {
+  if (handle === 0) {
+    inputMin.value = vulues[0];
+  } else {
+    inputMax.value = vulues[1];
+  }
+});
+
+inputMin.addEventListener("change", () => {
+  rangeSlider.noUiSlider.set([inputMin.value, null]);
+});
+inputMax.addEventListener("change", () => {
+  rangeSlider.noUiSlider.set([null, inputMax.value]);
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,113 +1,110 @@
-const { src, dest, watch, series, parallel } = require('gulp');
+const { src, dest, watch, series, parallel } = require("gulp");
 
-const browserSync = require('browser-sync').create();
-const scss = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify-es').default;
-const autoprefixer = require('gulp-autoprefixer');
-const clean = require('gulp-clean');
-const imagemin = require('gulp-imagemin');
-const webp = require('gulp-webp');
-const avif = require('gulp-avif');
-const newer = require('gulp-newer');
-const ttf2woff2 = require('gulp-ttf2woff2');
-const include = require('gulp-include');
-const svgstore = require('gulp-svgstore');
-
-
+const browserSync = require("browser-sync").create();
+const scss = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify-es").default;
+const autoprefixer = require("gulp-autoprefixer");
+const clean = require("gulp-clean");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
+const newer = require("gulp-newer");
+const ttf2woff2 = require("gulp-ttf2woff2");
+const include = require("gulp-include");
+const svgstore = require("gulp-svgstore");
 
 function sprites() {
-  return src('app/images/sprite/*.svg')
-  .pipe(svgstore())
-  .pipe(dest('app/images'))
+  return src("app/images/sprite/*.svg")
+    .pipe(svgstore())
+    .pipe(dest("app/images"));
 }
 
 function pages() {
-  return src('app/pages/*.html')
-    .pipe(include({
-      includePaths: ['app/components']
-    }))
-    .pipe(dest('app'))
-    .pipe(browserSync.stream())
+  return src("app/pages/*.html")
+    .pipe(
+      include({
+        includePaths: ["app/components"],
+      })
+    )
+    .pipe(dest("app"))
+    .pipe(browserSync.stream());
 }
 
-
-
 function fonts() {
-  return src('app/fonts/*.ttf')
-    .pipe(ttf2woff2())
-    .pipe(dest('app/fonts'))
+  return src("app/fonts/*.ttf").pipe(ttf2woff2()).pipe(dest("app/fonts"));
 }
 
 function images() {
-  return src(['app/images/src/*.*', '!app/images/src/*.svg'])
-    .pipe(newer('app/images'))
+  return src(["app/images/src/*.*", "!app/images/src/*.svg"])
+    .pipe(newer("app/images"))
     .pipe(avif({ quality: 50 }))
 
-    .pipe(src('app/images/src/*.*'))
-    .pipe(newer('app/images'))
+    .pipe(src("app/images/src/*.*"))
+    .pipe(newer("app/images"))
     .pipe(webp())
 
-    .pipe(src('app/images/src/*.*'))
-    .pipe(newer('app/images'))
+    .pipe(src("app/images/src/*.*"))
+    .pipe(newer("app/images"))
     .pipe(imagemin())
 
-    .pipe(dest('app/images'))
-
+    .pipe(dest("app/images"));
 }
-
 
 function styles() {
-  return src('app/scss/*.scss')
+  return src("app/scss/*.scss")
     .pipe(autoprefixer())
-    .pipe(concat('style.min.css'))
-    .pipe(scss({
-      style: 'compressed',
-    }))
-    .pipe(dest('app/css'))
+    .pipe(concat("style.min.css"))
+    .pipe(
+      scss({
+        style: "compressed",
+      })
+    )
+    .pipe(dest("app/css"))
     .pipe(browserSync.stream());
 }
-
 
 function scripts() {
   return src([
-    'node_modules/swiper/swiper-bundle.js',
-    'app/js/main.js',
+    "node_modules/swiper/swiper-bundle.js",
+    "node_modules/nouislider/dist/nouislider.js",
+    "app/js/main.js",
   ])
-    .pipe(concat('main.min.js'))
+    .pipe(concat("main.min.js"))
     .pipe(uglify())
-    .pipe(dest('app/js'))
+    .pipe(dest("app/js"))
     .pipe(browserSync.stream());
-}// Add your JS files here
+} // Add your JS files here
 
 function watching() {
   browserSync.init({
     server: {
-      baseDir: 'app'
+      baseDir: "app",
     },
   });
-  watch(['app/scss/*.scss'], styles);
-  watch(['app/js/main.js'], scripts);
-  watch(['app/images/src/*.*'], images);
-  watch(['app/images/sprite/*.*'], sprites);
-  watch(['app/components/*', 'app/pages/*'], pages);
-  watch(['app/*.html']).on('change', browserSync.reload);
+  watch(["app/scss/*.scss"], styles);
+  watch(["app/js/main.js"], scripts);
+  watch(["app/images/src/*.*"], images);
+  watch(["app/images/sprite/*.*"], sprites);
+  watch(["app/components/*", "app/pages/*"], pages);
+  watch(["app/*.html"]).on("change", browserSync.reload);
 }
 
 function bilding() {
-  return src([
-    'app/css/style.min.css',
-    'app/images/**/*.*',
-    'app/fonts/**/*.*',
-    'app/js/main.min.js',
-    'app/*.html',
-  ], { base: 'app' })
-    .pipe(dest('dist'));
+  return src(
+    [
+      "app/css/style.min.css",
+      "app/images/**/*.*",
+      "app/fonts/**/*.*",
+      "app/js/main.min.js",
+      "app/*.html",
+    ],
+    { base: "app" }
+  ).pipe(dest("dist"));
 }
 
 function cleanDist() {
-  return src('dist')
-    .pipe(clean());
+  return src("dist").pipe(clean());
 }
 
 exports.styles = styles;
